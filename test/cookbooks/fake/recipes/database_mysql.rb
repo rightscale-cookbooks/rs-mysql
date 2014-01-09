b70b55260d3d6f9da1b5740f44d1f80e433fc932
Nitin Mohan <nitin@rightscale.com>
2014-01-08 17:35:29 -0800

    Revert "Add kitchen test for verifying rs-mysql::client recipe"
    This reverts commit a8bf81d1bd9651be929ee889a2a377d8f1d85a63.

--- test/cookbooks/fake/recipes/database_mysql.rb
+++ test/cookbooks/fake/recipes/database_mysql.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-include_recipe 'rs-mysql::client'
+include_recipe 'database::mysql'
 
 # The connection hash to use to connect to mysql
 mysql_connection_info = {
