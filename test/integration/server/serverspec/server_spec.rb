6a818635977f4be270b845aef7f618002fed9cb1
Nitin Mohan <nitin@rightscale.com>
2014-01-08 17:47:58 -0800

    Add the database query tests to server_spec and remove the extra test

--- test/integration/server/serverspec/server_spec.rb
+++ test/integration/server/serverspec/server_spec.rb
@@ -36,3 +36,29 @@ end
 describe file('/var/lib/mysql') do
   it { should be_directory }
 end
+
+describe "can run MySQL queries on the server" do
+  describe "'app_test' database exists" do
+    describe command(
+      "echo \"SHOW DATABASES LIKE 'app_test'\" | mysql --user=root --password=rootpass"
+    ) do
+      it { should return_stdout /app_test/ }
+    end
+  end
+
+  describe "select tables from a database" do
+    describe command(
+      "echo \"USE app_test; SELECT * FROM app_test\" | mysql --user=root --password=rootpass"
+    ) do
+      it { should return_stdout /I am in the db/ }
+    end
+  end
+
+  describe "create database" do
+    describe command(
+      "echo \"CREATE DATABASE IF NOT EXISTS blah; SHOW DATABASES LIKE 'blah'\" | mysql --user=root --password=rootpass"
+    ) do
+      it { should return_stdout /blah/ }
+    end
+  end
+end
