f6d1ff01bfcb510acecda26c11db568698af6b9f
Douglas Thrift <douglas.thrift@rightscale.com>
2014-01-08 16:33:39 -0800

    Initial rspec for tuning attributes.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,6 @@ group :integration do
   gem 'test-kitchen', '~> 1.1.0'
   gem 'kitchen-vagrant'
   gem 'strainer', '~> 3.3.0'
-  gem 'chefspec', '~> 3.0.2'
+  gem 'chefspec', '~> 3.1.0'
   gem 'travis-lint'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,3 +21,9 @@ libraries_path = File.expand_path('../../libraries', __FILE__)
 $LOAD_PATH.unshift(libraries_path) unless $LOAD_PATH.include?(libraries_path)
 
 require 'chefspec'
+require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  config.platform = 'ubuntu'
+  config.version = '12.04'
+end
