f269bae67a9b95e90b36f81c528a6ef3cbf95662
Kannan Manickam <kannan@rightscale.com>
2014-01-14 16:00:26 -0800

    Downgrade database to match rs-application_php

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'marker', '~> 1.0.0'
-depends 'database', '~> 1.6.0'
+depends 'database', '~> 1.5.2'
 depends 'mysql', '~> 4.0.18'
 depends 'collectd', '~> 1.1.0'
 
