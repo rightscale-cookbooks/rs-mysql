e14f4f890166948968a7f72e1104960cd5cabcef
Kannan Manickam <kannan@rightscale.com>
2014-01-15 14:21:16 -0800

    Fixed the failing shared-server tests.

--- recipes/server.rb
+++ recipes/server.rb
@@ -56,15 +56,15 @@ collectd_plugin "mysql" do
   })
 end
 
+# The connection hash to use to connect to mysql
+mysql_connection_info = {
+  :host => 'localhost',
+  :username => 'root',
+  :password => node['rs-mysql']['server_root_password']
+}
+
 if node['rs-mysql']['application_username'] && node['rs-mysql']['application_password']
   raise 'The rs-mysql/database_name is required for creating user' unless node['rs-mysql']['database_name']
-
-  # The connection hash to use to connect to mysql
-  mysql_connection_info = {
-    :host => 'localhost',
-    :username => 'root',
-    :password => node['rs-mysql']['server_root_password']
-  }
 
   # Create the application user
   mysql_database_user node['rs-mysql']['application_username'] do
@@ -75,10 +75,11 @@ if node['rs-mysql']['application_username'] && node['rs-mysql']['application_pas
     privileges node['rs-mysql']['application_user_privileges']
     action [:create, :grant]
   end
+end
 
-  # Create the database
-  mysql_database node['rs-mysql']['database_name'] do
-    connection mysql_connection_info
-    action :create
-  end
+# Create the database
+mysql_database node['rs-mysql']['database_name'] do
+  only_if { node['rs-mysql']['database_name'] }
+  connection mysql_connection_info
+  action :create
 end
