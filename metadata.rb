1084107ff71b9691668a21dab640c70315cca853
Kannan Manickam <kannan@rightscale.com>
2014-01-22 14:33:06 -0800

    Bump version to 1.0.0. [skip ci]

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Installs and configures a MySQL server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'marker', '~> 1.0.0'
 depends 'database', '~> 1.5.2'
