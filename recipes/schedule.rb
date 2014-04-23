cd49c4b8eff75eb9d048be847332e033cabc4e01
Kannan Manickam <kannan@rightscale.com>
2014-04-22 17:17:30 -0700

    Finished stripe and schedule recipes.

--- recipes/schedule.rb
+++ recipes/schedule.rb
@@ -16,3 +16,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+marker "recipe_start_rightscale" do
+  template "rightscale_audit_entry.erb"
+end
+
+schedule_enable = node['rs-mysql']['schedule']['enable'] == true || node['rs-mysql']['schedule']['enable'] == 'true'
+schedule_hour = node['rs-mysql']['schedule']['hour']
+schedule_minute = node['rs-mysql']['schedule']['minute']
+lineage = node['rs-mysql']['backup']['lineage']
+
+# Both schedule hour and minute should be set
+unless schedule_hour && schedule_minute
+  raise 'rs-mysql/schedule/hour and rs-mysql/schedule/minute inputs should be set'
+end
+
+# Adds or removes the crontab entry for backup schedule based on rs-mysql/schedule/enable
+cron "backup_schedule_#{lineage}" do
+  minute schedule_minute
+  hour schedule_hour
+  command "rs_run_recipe --policy 'rs-mysql::backup' --name 'rs-mysql::backup'"
+  action schedule_enable ? :create : :delete
+end
--- recipes/stripe.rb
+++ recipes/stripe.rb
@@ -16,3 +16,102 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+marker "recipe_start_rightscale" do
+  template "rightscale_audit_entry.erb"
+end
+
+device_count = node['rs-mysql']['device']['count'].to_i
+nickname = node['rs-mysql']['device']['nickname']
+size = node['rs-mysql']['device']['volume_size'].to_i
+
+raise 'rs-mysql/device/count should be at least 2 for setting up stripe' if device_count < 2
+
+detach_timeout = node['rs-mysql']['device']['detach_timeout'].to_i * device_count
+
+execute "set decommission timeout to #{detach_timeout}" do
+  command "rs_config --set decommission_timeout #{detach_timeout}"
+  not_if "[ `rs_config --get decommission_timeout` -eq #{detach_timeout} ]"
+end
+
+each_device_size = (size.to_f / device_count.to_f).ceil
+
+Chef::Log.info "Total size is: #{size}"
+Chef::Log.info "Device count in logical volume is set to: #{device_count}"
+Chef::Log.info "Each device in the logical volume will be created of size: #{each_device_size}"
+
+device_nicknames = []
+
+# Cloud-specific volume options
+volume_options = {}
+volume_options[:iops] = node['rs-mysql']['device']['iops'] if node['rs-mysql']['device']['iops']
+
+# Install packages required for setting up LVM
+include_recipe 'lvm::default'
+
+# rs-mysql/restore/lineage is empty, creating new volume(s) and setting up LVM
+if node['rs-mysql']['restore']['lineage'].to_s.empty?
+  1.upto(device_count) do |device_num|
+    device_nicknames << "#{nickname}_#{device_num}"
+    rightscale_volume "#{nickname}_#{device_num}" do
+      size each_device_size
+      options volume_options
+      action [:create, :attach]
+    end
+  end
+# rs-mysql/restore/lineage is set, restore from the backup
+else
+  rightscale_backup nickname do
+    lineage node['rs-mysql']['restore']['lineage']
+    timestamp node['rs-mysql']['restore']['timestamp'].to_i if node['rs-mysql']['restore']['timestamp']
+    size each_device_size
+    options volume_options
+    action :restore
+  end
+end
+
+# Remove any characters other than alphanumeric and dashes and replace with dashes
+sanitized_nickname = nickname.downcase.gsub(/[^-a-z0-9]/, '-')
+
+# Setup LVM on the volumes. The following resources will:
+#   - initialize the physical volumes for use by LVM
+#   - create volume group and logical volume
+#   - format and mount the logical volume
+lvm_volume_group "#{sanitized_nickname}-vg" do
+  physical_volumes(lazy do
+    if node['rs-mysql']['restore']['lineage'].to_s.empty?
+      device_nicknames.map { |nickname| node['rightscale_volume'][nickname]['device'] }
+    else
+      node['rightscale_backup'][nickname]['devices']
+    end
+  end)
+end
+
+lvm_logical_volume "#{sanitized_nickname}-lv" do
+  group "#{sanitized_nickname}-vg"
+  size '100%VG'
+  filesystem node['rs-mysql']['device']['filesystem']
+  mount_point node['rs-mysql']['device']['mount_point']
+  stripes device_count
+  stripe_size node['rs-mysql']['device']['stripe_size']
+end
+
+new_mysql_dir = "#{node['rs-mysql']['device']['mount_point']}/mysql"
+
+# Make sure that there is a 'mysql' directory on the mount point of the volume
+directory new_mysql_dir do
+  owner 'mysql'
+  group 'mysql'
+  mode '0755'
+  action :create
+end
+
+# Override the mysql data_dir. This will do the following:
+#   - Change the data_dir setting in the my.cnf to the new location.
+#   - Move the data from the /var/lib/mysql to this new location. This will be done only if the new location is
+#     empty.
+node.override['mysql']['data_dir'] = new_mysql_dir
+node.override['mysql']['server']['directories']['log_dir'] = new_mysql_dir
+
+# Include the rs-mysql::default so
+include_recipe 'rs-mysql::default'
