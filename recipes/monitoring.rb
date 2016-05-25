ab4374d671adb8692bf930f9ba4e87d7ee875ef1
Richard Shade <rshade@rightscale.com>
2016-05-25 10:40:47 -0500

    adding monitoring scripts for slave

---
+++ recipes/monitoring.rb
@@ -0,0 +1,35 @@
+user 'mysql_monitor' do
+  action :create
+end
+
+template '/usr/local/bin/mysql_seconds_behind_master.rb' do
+  source 'mysql_seconds_behind_master.rb.erb'
+  owner 'root'
+  group 'root'
+  mode 0755
+  action :create
+end
+
+node.set['kabam-rs-mysql']['exec'] = ['Exec "mysql_monitor" "/opt/chef/embedded/bin/ruby" "/usr/local/bin/mysql_seconds_behind_master.rb"']
+
+template '/usr/local/bin/mysql_slave_running.rb' do
+  source 'mysql_slave_running.rb.erb'
+  owner 'root'
+  group 'root'
+  mode 0755
+  action :create
+end
+
+node.set['kabam-rs-mysql']['exec'] << 'Exec "mysql_monitor" "/opt/chef/embedded/bin/ruby" "/usr/local/bin/mysql_slave_running.rb"'
+
+template ::File.join('/etc/collectd/plugins', 'exec.conf') do
+  source 'exec.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0755
+  action :create
+end
+
+service 'collectd' do
+  action :restart
+end
